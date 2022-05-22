b89a82453dff9c60cabeba3af2e661d3da066a7d
YJ <youngjo9486@gmail.com>
2022-05-22 09:35:13 +0900

    주석

---
+++ ps0.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 22 09:10:31 2022
+
+@author: young
+"""
+import math
+
+
+x= float(input("Enter number x : "))
+y= float(input("Enter number y : "))
+
+print("x**y = {}".format(x**y))
+print("log(x) = {}".format(math.log(x)))
+
+input("")
+      
+     
